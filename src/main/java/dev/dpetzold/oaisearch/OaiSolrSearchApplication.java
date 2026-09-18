235c4a453c49f8ee4dfb967b6e904d5b8e236fac
Deniz Petzold <mail@dpetzold.dev>
2026-09-18 19:47:11 +0200

    initial spring java maven structure

---
+++ src/main/java/dev/dpetzold/oaisearch/OaiSolrSearchApplication.java
@@ -0,0 +1,13 @@
+package dev.dpetzold.oaisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaiSolrSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaiSolrSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dpetzold/oaisearch/OaiSolrSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dpetzold.oaisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaiSolrSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
